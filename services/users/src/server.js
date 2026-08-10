35f30e6715baa8fe37144ba1afd28d65be5ded6e
gabrielmoc <gabrielmoc2010@hotmail.com>
2026-08-10 18:25:03 -0300

    feat: implementar users service

--- services/users/src/server.js
+++ services/users/src/server.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = Number(process.env.USERS_SERVICE_PORT || 3002);
+const users = require(path.resolve(
+  __dirname,
+  "../../../shared/datasets/users.json"
+));
+
+app.get("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const user = users.find((entry) => entry.id === userId);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  return res.json(user);
+});
 
 app.get("/health", (_req, res) => {
   res.json({
